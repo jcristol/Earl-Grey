d271543081ec7794a9848fd7da80f9974b61f530
jcristol <joshcristol@gmail.com>
2018-06-28 17:49:14 -0500

    formatting

--- test/tensor.spec.ts
+++ test/tensor.spec.ts
@@ -2,35 +2,42 @@ import { Tensor } from "../src/ts/tensor";
 import { expect } from "chai";
 import "mocha";
 
-const std_dimensions: Array<number> = [2,3];
+const std_dimensions: Array<number> = [2, 3];
 
 function make_tensor_array(func: Function, d: Array<number>): Array<any> {
-  if(d.length > 0){
-    return Array(d[0]).fill(null).map(() => make_tensor_array(func, d.slice(1)));
-  } else{
+  if (d.length > 0) {
+    return Array(d[0])
+      .fill(null)
+      .map(() => make_tensor_array(func, d.slice(1)));
+  } else {
     return func();
   }
 }
 
 function validate_dimensions(t: Tensor, d: Array<number>): boolean {
-  if(t.data instanceof Array) {
-    const valid_children: boolean = t.data.every(child => validate_dimensions(child, d.slice(1)))
+  if (t.data instanceof Array) {
+    const valid_children: boolean = t.data.every(child =>
+      validate_dimensions(child, d.slice(1))
+    );
     return valid_children && d[0] === t.dimensions[0];
   }
   return t.dimensions.length === d.length;
 }
 
 function validate_copy(a: Tensor, b: Tensor): boolean {
-  if(a.data instanceof Array && b.data instanceof Array ) {
-    const valid_children_a: boolean = a.data.every((child, index) => validate_copy(child, b.data[index]));
-    const valid_children_b: boolean = b.data.every((child, index) => validate_copy(child, a.data[index]));
-    return valid_children_a && valid_children_b
+  if (a.data instanceof Array && b.data instanceof Array) {
+    const valid_children_a: boolean = a.data.every((child, index) =>
+      validate_copy(child, b.data[index])
+    );
+    const valid_children_b: boolean = b.data.every((child, index) =>
+      validate_copy(child, a.data[index])
+    );
+    return valid_children_a && valid_children_b;
   }
   return a.data === b.data;
 }
 
 describe("test tensor utility functions", () => {
-
   it("validate dimensions", () => {
     const a: Tensor = Tensor.zeros(...std_dimensions);
     expect(validate_dimensions(a, std_dimensions)).to.be.true;
@@ -39,13 +46,13 @@ describe("test tensor utility functions", () => {
   it("validate copy is exactly the same for all zeros", () => {
     const a: Tensor = Tensor.zeros(...std_dimensions);
     const b: Tensor = Tensor.zeros(...std_dimensions);
-    expect(validate_copy(a,b)).to.be.true;
+    expect(validate_copy(a, b)).to.be.true;
   });
 
   it("validate copy is distinct from src", () => {
     const a: Tensor = Tensor.zeros(...std_dimensions);
     const b: Tensor = Tensor.zeros(...std_dimensions).map(() => 1);
-    expect(validate_copy(a,b)).to.be.false;
+    expect(validate_copy(a, b)).to.be.false;
   });
 
   it("validate map and toArray", () => {
@@ -53,5 +60,4 @@ describe("test tensor utility functions", () => {
     const a: Tensor = Tensor.zeros(...std_dimensions).map(foo);
     expect(a.toArray()).to.be.deep.equal(make_tensor_array(foo, a.dimensions));
   });
-
 });
