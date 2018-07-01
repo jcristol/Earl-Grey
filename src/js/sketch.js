012e310c1f2e3689b4ae1acfd31e46f47cf30123
jcristol <joshcristol@gmail.com>
2018-07-01 13:24:50 -0500

    sketch tester

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -6,15 +6,18 @@ const structure = {
   alpha: 0.01,
   hidden_layers: 1,
   input_nodes: 2,
-  hidden_nodes: 3,
-  output_nodes: 2
+  hidden_nodes: 2,
+  output_nodes: 1
 };
 
 window.setup = function() {
   let nn = new NeuralNetwork(structure);
   let input = [1, 0];
-  let output = nn.test(input);
+  let target = [1];
+  console.log("before: ", nn);
+  nn.train(input, target);
+  console.log("after", nn);
+  // let output = nn.test(input);
   // let targets = [1,0];
   // nn.train(inputs, targets);
-  console.log(output);
 };
