f0062f4b7b8e548af723b8503587a0c1c6f726bc
jcristol <joshcristol@gmail.com>
2018-07-02 14:41:19 -0500

    fresh

--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const Index = () => {
-  console.log("hello world");
-  return <div>Hello React!</div>;
-};
-
-ReactDOM.render(<Index />, document.getElementById("index"));
